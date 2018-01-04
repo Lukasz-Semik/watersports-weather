2a3cd27f2e77459bceb86535d3516b3ce286c54a
djpluki <djpluki@gmail.com>
2018-01-04 23:28:10 +0100

    api call improvement

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 
 import { API_KEY_DEV } from '../../API_KEY_DEV';
 
@@ -20,47 +21,68 @@ class Dashboard extends Component{
 
   componentDidMount(){
     const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?';
-    if('geolocation' in navigator){
-      navigator.geolocation.getCurrentPosition(position=>{
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        console.log(lat, lon);
-        const query = `${API_URL}lat=${lat}&lon=${lon}&APPID=${API_KEY_DEV}`
-        axios.get(query)
-          .then(response=>{
-            console.log(response.data);
-            const weatherData = prepareWeatherData(response.data.list);
-            this.setState({
-              weatherByGeolocationFetched: true,
-              userCity: response.data.city.name,
-              userCountry: response.data.city.country,
-              weatherData
-            }) 
-          })
-          .then(()=>{
-            this.putCityInfoToLocalStorage();
-          })
-      },error=>{
-        console.log(error);
-        this.setState({geolocationDeniedOrErr: true})
-      },{
-        //geolocation options
-        enableHighAccuracy: true,
-        maximumAge: 30000,
-        timeout: 27000
-      })
+    let lastFetchTime;
+    try{
+      lastFetchTime = JSON.parse(localStorage.getItem('timeOfWeatherForStorage'));
+    }catch(e){
+      lastFetchTime = null;
+    }
+    // Block of code created to be compliant with 
+    // API and do not trigger the same req more often than 10 minutes
+    const storageTime = moment(lastFetchTime).add(10,'m')
+    const currentTime = moment()
+    const isDelayedEnough = !lastFetchTime ? true : storageTime.isBefore(currentTime,'m');
+    console.log(isDelayedEnough);
+    if(!isDelayedEnough){
+      console.log('we cannot fetch');
+    }
+    if('geolocation' in navigator && isDelayedEnough){
+      console.log('we can fetch');
+      this.geolocationFetching(API_URL);
       return; 
     }
     this.setState({geolocationDeniedOrErr: true})
   }
 
+  geolocationFetching(apiUrl){
+    navigator.geolocation.getCurrentPosition(position=>{
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      console.log(lat, lon);
+      const query = `${apiUrl}lat=${lat}&lon=${lon}&APPID=${API_KEY_DEV}`
+      axios.get(query)
+        .then(response=>{
+          console.log(response.data);
+          const weatherData = prepareWeatherData(response.data.list);
+          this.setState({
+            weatherByGeolocationFetched: true,
+            userCity: response.data.city.name,
+            userCountry: response.data.city.country,
+            weatherData
+          }) 
+        })
+        .then(()=>{
+          this.putCityInfoToLocalStorage();
+        })
+    },error=>{
+      console.log(error);
+      this.setState({geolocationDeniedOrErr: true})
+    },{
+      //geolocation options
+      enableHighAccuracy: true,
+      maximumAge: 30000,
+      timeout: 27000
+    })
+  };
+
   putCityInfoToLocalStorage(){
     if(!localStorage)
       return;
-    const {userCity, userCountry} = this.state;
-    const userCityInfo = JSON.stringify({userCity, userCountry});
+    const weatherDataForStorage = JSON.stringify({...this.state});
+    const timeOfWeatherForStorage = JSON.stringify(moment());
     try{
-      localStorage.setItem('userCityInfo',userCityInfo);
+      localStorage.setItem('weatherDataForStorage', weatherDataForStorage);
+      localStorage.setItem('timeOfWeatherForStorage', timeOfWeatherForStorage);
     }catch(e){
       //TO DO: notification. 
       console.log(e);
--- app/src/functions/prepareWeatherData.js
+++ app/src/functions/prepareWeatherData.js
@@ -1,10 +1,13 @@
+import moment from 'moment';
+
 function prepareWeatherData(fetchedWeather=[]){
+  console.log(moment('2018-01-04 00:00').isBetween('2018-01-03 00:00','2018-01-06','day'));
   if(fetchedWeather.length<1)
     return;
   return fetchedWeather.map(weatherItem=>{
     const notFetched = 'not fetched';
     return{
-      timeStamp: weatherItem.dt ? weatherItem.dt : notFetched,
+      time: weatherItem.dt ? moment.unix(weatherItem.dt) : notFetched,
       temperature: weatherItem.main ? weatherItem.main.temp-275.15 : notFetched,
       pressure: weatherItem.main ? weatherItem.main.pressure : notFetched,
       rain: weatherItem.rain ? weatherItem.rain['3h'] : notFetched,
